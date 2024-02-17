99539641fc7ea3c98c7e41408db10024961620b5
Emre <98480453+mrkrgz@users.noreply.github.com>
2024-02-17 04:31:59 +0300

    initial commit

---
+++ Triangle Area Calculator.py
@@ -0,0 +1,12 @@
+#### Triangle area calculator
+
+prompt="Enter the base length and height of the triangle in centimeters respectively:"
+
+input1=float(input(prompt))
+print("Length of the base:",input1,)
+input2=float(input())
+print("Height:",input2,)
+
+Area = (input1 * input2) / 2
+
+print("Area:",Area,end="cm²",)
