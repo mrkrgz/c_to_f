1ac0ef36393380c88292e9bb4ef49e80a5313cb5
Emre <98480453+mrkrgz@users.noreply.github.com>
2024-02-17 04:30:11 +0300

    initial commit

---
+++ C to F.py
@@ -0,0 +1,8 @@
+##### °C to °F
+prompt="Enter temperature value (in Celsius):"
+
+C=float(input(prompt))
+fahrenheit=(C*1.8+32)
+
+print("In Celsius:",round(C),"°C")
+print("In Fahrenheit:",round(fahrenheit),"°F")
